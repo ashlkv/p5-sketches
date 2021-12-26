014a006e1335712022a84243cd079feee20854bb
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2021-12-26 17:59:11 +0530

    Grid with black and white dots

--- noise-grid/noise-grid.js
+++ noise-grid/noise-grid.js
@@ -1,18 +1,24 @@
-const noiseIncrement = 0.1;
 const cellSize = 20;
 const width = 600;
 const height = 700;
+let grid = []
 
 function setup() {
     createCanvas(width, height);
+    grid = [...new Array(height / cellSize + 1)].map(() => [...new Array(width / cellSize + 1)].map(() => floor(random(2))));
 }
 
 function draw() {
-    for (let row = 0; row < height / cellSize; row++) {
-        for (let column = 0; column < width / cellSize; column++) {
-            const perlinNoiseFill = noise(column * noiseIncrement, row * noiseIncrement) * 255;
-            fill(perlinNoiseFill);
-            rect(column * cellSize, row * cellSize, cellSize, cellSize);
+    background(122);
+    let rowIndex = -1;
+    for (const row of grid) {
+        rowIndex ++;
+        let columnIndex = -1;
+        for (const column of row) {
+            columnIndex ++;
+            stroke(column * 255);
+            strokeWeight(4);
+            point(columnIndex * cellSize, rowIndex * cellSize, 255)
         }
     }
 }
