a2c9a79648ca9ec1d7d60e16577c0f3114a448b1
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2021-12-27 13:48:56 +0530

    Lines around black dot isles

--- noise-grid/noise-grid.js
+++ noise-grid/noise-grid.js
@@ -10,15 +10,70 @@ function setup() {
 
 function draw() {
     background(122);
-    let rowIndex = -1;
-    for (const row of grid) {
-        rowIndex ++;
-        let columnIndex = -1;
-        for (const column of row) {
-            columnIndex ++;
-            stroke(column * 255);
+    for (let row = 0; row < grid.length - 1; row++) {
+        for (let column = 0; column < grid[row].length - 1; column++) {
+            stroke(grid[row][column] * 255);
             strokeWeight(4);
-            point(columnIndex * cellSize, rowIndex * cellSize, 255)
+            point(column * cellSize, row * cellSize, 255)
         }
     }
+    
+    for (let row = 0; row < grid.length - 1; row ++) {
+        for (let column = 0; column < grid[row].length - 1; column ++) {
+            // .___a___.
+            // |       |
+            // d       b
+            // |       |
+            // .___c___.
+            const a = new p5.Vector(column * cellSize + cellSize / 2, row * cellSize);
+            const b = new p5.Vector(column * cellSize + cellSize, row * cellSize + cellSize / 2)
+            const c = new p5.Vector(column * cellSize + cellSize / 2, row * cellSize + cellSize);
+            const d = new p5.Vector(column * cellSize, row * cellSize + cellSize / 2)
+            const binaryNumber = `${grid[row][column]}${grid[row][column + 1]}${grid[row + 1][column + 1]}${grid[row + 1][column]}`;
+            const decimalNumber = parseInt(binaryNumber, 2)
+            stroke(255);
+            strokeWeight(1);
+            switch (decimalNumber) {
+                case 0:
+                case 15:
+                    break;
+                case 1:
+                case 14:
+                    connectVectors(c, d);
+                    break;
+                case 2:
+                case 13:
+                    connectVectors(b, c);
+                    break;
+                case 3:
+                case 12:
+                    connectVectors(b, d);
+                    break;
+                case 4:
+                case 11:
+                    connectVectors(a, b);
+                    break;
+                case 5:
+                    connectVectors(b, c);
+                    connectVectors(d, a);
+                    break;
+                case 6:
+                case 9:
+                    connectVectors(a, c)
+                    break;
+                case 7:
+                case 8:
+                    connectVectors(d, a)
+                    break;
+                case 10:
+                    connectVectors(a, b)
+                    connectVectors(c, d)
+                    break;
+            }
+        }
+    }
+}
+
+const connectVectors = (fromVector, toVector) => {
+    return line(fromVector.x, fromVector.y, toVector.x, toVector.y);
 }
