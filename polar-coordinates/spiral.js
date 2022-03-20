d44258dc461b198f4a10660397e51a7188a81f42
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2022-03-20 06:55:26 +0100

    Circle and spiral in polar coordinates

---
+++ polar-coordinates/spiral.js
@@ -0,0 +1,22 @@
+window.P5 = p5;
+
+new p5((p5) => {
+    const width = 600;
+    const height = 600;
+    let pathRadius = 100;
+    let angle = 0;
+
+    p5.setup = () => {
+        p5.createCanvas(width, height);
+        p5.background(0);
+    }
+
+    p5.draw = () => {
+        p5.translate(width / 2, height / 2)
+        p5.noStroke();
+        p5.fill(255);
+        p5.ellipse(p5.cos(angle) * pathRadius, p5.sin(angle) * pathRadius, 10, 10);
+        angle += 0.01;
+        pathRadius -= 0.05;
+    }
+}, document.querySelector('main'));
