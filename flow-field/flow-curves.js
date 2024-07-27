9b111b067c81694c487db835aa47cca3e3aa8211
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2024-07-27 16:40:30 +0200

    Curve debug

--- flow-field/flow-curves.js
+++ flow-field/flow-curves.js
@@ -9,19 +9,19 @@ function FlowLine(p5, {column, row, noiseIncrement, cellSize, visible = false}) 
     
     this.update = (time = undefined) => {
         // Using PI will make the field less wavy
-        this.angle = p5.noise(column * noiseIncrement, row * noiseIncrement, time) * p5.TWO_PI * 4;
+        this.angle = p5.noise(column * noiseIncrement, row * noiseIncrement, time) * p5.TWO_PI * 3;
         this.vector = P5.Vector.fromAngle(this.angle, this.radius)
         this.oppositeVector = P5.Vector.fromAngle(this.angle + p5.PI, this.radius);
         if (visible) {
             const { x: x1, y: y1 } = this.anchor;
             const { x: x2, y: y2 } = { x: this.anchor.x + this.vector.x, y: this.anchor.y + this.vector.y }
             p5.strokeWeight(1)
-            p5.stroke(0, 100);
+            p5.stroke(255, 0, 0, 50);
             p5.line(x1, y1, x2, y2)
             
             // Dot at the end
             p5.strokeWeight(3)
-            p5.stroke(0, 100);
+            p5.stroke(255, 0, 0, 50);
             p5.point(x2, y2)
         }
     }
@@ -38,8 +38,11 @@ function FlowField(p5, { width, height, noiseIncrement, cellSize, visible }) {
     
     /** Gets closest vector to the point specified in pixels */
     this.getVectorAt = ({x, y}) => {
-        const column = Math.min(Math.max(Math.round(x / cellSize), 0), width - 1);
-        const row = Math.min(Math.max(Math.round(y / cellSize), 0), height - 1);
+        const column = Math.round(x / cellSize);
+        const row = Math.round(y / cellSize);
+        if (column < 0 || row < 0 || column > width - 1 || row > height - 1) {
+            return undefined;
+        }
         return this.lines[row][column].vector
     }
 }
@@ -50,6 +53,9 @@ function Curve(p5, { flowField, start, steps, step = 10 }) {
         const point = this.vertices[i];
         const vector = p5.createVector(point.x, point.y);
         const force = flowField.getVectorAt(point);
+        if (!force) {
+            break;
+        }
         force.setMag(step);
         vector.add(force)
         this.vertices.push({x: vector.x, y: vector.y})
@@ -58,26 +64,26 @@ function Curve(p5, { flowField, start, steps, step = 10 }) {
 
 new p5((p5) => {
     const cellSize = 15;
-    const noiseIncrement = 0.01;
+    const noiseIncrement = 0.05;
     const canvasSize = { width: Math.floor(window.innerWidth / cellSize) * cellSize, height: Math.floor(window.innerHeight / cellSize) * cellSize }
     let time = 0;
     let flowField;
 
     p5.setup = () => {
         p5.frameRate(5)
-        p5.createCanvas(canvasSize.width, canvasSize.height);
+        p5.createCanvas(canvasSize.width, canvasSize.height, p5.SVG);
         
-        flowField = new FlowField(p5, {width: canvasSize.width / cellSize, height: canvasSize.height / cellSize, noiseIncrement, cellSize, visible: false})
+        flowField = new FlowField(p5, {width: canvasSize.width / cellSize, height: canvasSize.height / cellSize, noiseIncrement, cellSize, visible: true})
         window.flowField = flowField
     }
 
     p5.draw = () => {
-        Array(1000).fill().forEach((element, index) => {
+        Array(100).fill().forEach((element, index) => {
             const start = {
                 x: p5.random(canvasSize.width),
                 y: p5.random(canvasSize.height)
             }
-            const curve = new Curve(p5, { start, steps: 1000, flowField, step: 30 })
+            const curve = new Curve(p5, { start, steps: 10, flowField, step: 30 })
             
             p5.strokeWeight(1)
             p5.stroke(0, 0, 0, 100);
@@ -92,13 +98,18 @@ new p5((p5) => {
             })
             p5.endShape();
             
-            // p5.strokeWeight(3)
-            // p5.stroke(0, 0, 0, 255);
-            // curve.vertices.forEach(({x, y}) => {
-            //     p5.point(x, y)
-            // })
+            /*p5.strokeWeight(3)
+            p5.stroke(255, 0, 0, 255);
+            p5.point(curve.vertices[0].x, curve.vertices[0].y);*/
+            
+            p5.strokeWeight(3)
+            p5.stroke(0, 0, 0, 255);
+            curve.vertices.forEach(({x, y}) => {
+                p5.point(x, y)
+            })
         })
         
+        // p5.save('flow-curves.svg')
         p5.noLoop();
     }
 }, document.querySelector('main'));
