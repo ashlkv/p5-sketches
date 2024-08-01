3c669ba355cf6763407e402c8c3836303ae456dd
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2024-08-01 13:34:40 +0200

    Flow splines

--- flow-field/flow-curves.js
+++ flow-field/flow-curves.js
@@ -38,15 +38,41 @@ function FlowField(p5, { width, height, noiseIncrement, cellSize }) {
     this.values = Array(height).fill()
                     .map((value, row) => Array(width).fill()
                         .map((value, column) => p5.noise(column * noiseIncrement, row * noiseIncrement) * p5.TWO_PI * 3))
+    const cellDiagonal = p5.dist(0, 0, cellSize, cellSize);
+    
+    const isInBounds = ({ column, row }, {width, height}) => {
+        return column >= 0 && row >= 0 && column <= width - 1 && row <= height - 1
+    }
     
     /** Gets closest vector to the point specified in pixels */
     this.getValueAt = ({x, y}) => {
         const column = Math.round(x / cellSize);
         const row = Math.round(y / cellSize);
-        if (column < 0 || row < 0 || column > width - 1 || row > height - 1) {
+        if (!isInBounds({ column, row }, { width, height })) {
             return undefined;
         }
         return this.values[row][column]
+    }
+    
+    this.getWeightedAverageAt = ({x, y}) => {
+        const column = x / cellSize;
+        const row = y / cellSize;
+        const anchor1 = { row: Math.floor(row), column: Math.floor(column) }
+        const anchor2 = { row: Math.floor(row), column: Math.ceil(column) }
+        const anchor3 = { row: Math.ceil(row), column: Math.ceil(column) }
+        const anchor4 = { row: Math.ceil(row), column: Math.floor(column) }
+        const values = [anchor1, anchor2, anchor3, anchor4]
+            .filter(({ column, row }) => isInBounds({ column, row }, { width, height }))
+            .map(({ column, row }) => {
+                const value = this.values[row][column];
+                const distance = p5.dist(x, y, column * cellSize, row * cellSize);
+                return { value, weight: cellDiagonal - distance }
+            })
+        if (values.length === 0) {
+            return undefined
+        }
+        const weights = values.reduce((sum, {weight}) => sum + weight, 0)
+        return values.reduce((sum, {value, weight}) => sum + value * weight, 0) / weights
     }
 }
 
@@ -55,8 +81,8 @@ function Curve(p5, { flowField, start, steps, step = 10 }) {
     for (let i = 0; i < steps; i++) {
         const point = this.vertices[i];
         const vector = p5.createVector(point.x, point.y);
-        const value = flowField.getValueAt(point);
-        if (!value) {
+        const value = flowField.getWeightedAverageAt(point);
+        if (value === undefined) {
             break;
         }
         const force = P5.Vector.fromAngle(value, step)
@@ -66,8 +92,8 @@ function Curve(p5, { flowField, start, steps, step = 10 }) {
 }
 
 new p5((p5) => {
-    const cellSize = 50;
-    const noiseIncrement = 0.05;
+    const cellSize = 20;
+    const noiseIncrement = 0.009;
     const canvasSize = { width: Math.floor(window.innerWidth / cellSize) * cellSize, height: Math.floor(window.innerHeight / cellSize) * cellSize }
     let flowField;
     window.save = (name) => p5.save(name)
@@ -85,10 +111,12 @@ new p5((p5) => {
             line.render()
         }))
         
-        const startingPoints = sample(1, canvasSize.width, canvasSize.height);
+        const startingPoints = sample(2000, canvasSize.width, canvasSize.height);
+        // const startingPoints = [{x: Math.round(p5.random(canvasSize.width)), y: Math.round(p5.random(canvasSize.height))}];
         startingPoints.forEach((start) => {
-            const curve = new Curve(p5, { start, steps: 10, flowField, step: cellSize })
+            const curve = new Curve(p5, { start, steps: 10, flowField, step: cellSize - 5 })
             p5.strokeWeight(1)
+            // p5.stroke(244, 85, 49, 100);
             p5.stroke(0, 0, 0, 100);
             p5.fill(0, 0)
             p5.beginShape();
@@ -100,16 +128,18 @@ new p5((p5) => {
                 }
             })
             p5.endShape();
-            
+            /*
+            // Dots at vertices
             p5.strokeWeight(3)
             p5.stroke(0, 0, 0, 255);
             curve.vertices.forEach(({x, y}) => {
                 p5.point(x, y)
             })
             
+            // Red dot at start
             p5.strokeWeight(5)
             p5.stroke(255, 0, 0, 255);
-            p5.point(curve.vertices[0].x, curve.vertices[0].y)
+            p5.point(curve.vertices[0].x, curve.vertices[0].y)*/
         })
         
         p5.noLoop();
